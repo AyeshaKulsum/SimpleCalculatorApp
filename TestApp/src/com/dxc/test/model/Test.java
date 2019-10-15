152cf3ec8d89748ac2e50b3432976e6fd87eba93
Ayesha Kulsum S J <36045201+AyeshaKulsum@users.noreply.github.com>
2019-10-15 10:10:49 +0530

    Add files via upload

---
+++ TestApp/src/com/dxc/test/model/Test.java
@@ -0,0 +1,9 @@
+package com.dxc.test.model;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ TestApp/src/com/dxc/test/servlet/BeginServelet.java
@@ -0,0 +1,40 @@
+package com.dxc.test.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class BeginServelet
+ */
+public class BeginServelet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BeginServelet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name= request.getParameter("name");
+		HttpSession session=request.getSession();
+		session.setAttribute("myname", name);
+		response.getWriter().println("Instructions");
+		response.getWriter().println("\nTotal : 5 Questions ");
+		response.getWriter().println("\nTotal Marks : 100");
+		response.getWriter().println("\n<a href='DisplayServelet'><button>Begin Test</button></a>");
+		
+
+		 
+	}
+
+}
